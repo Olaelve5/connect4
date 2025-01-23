7397405e36e48aca73724a4b6ccc3f37f706cb0e
Ola Johannes Elvedahl <olaelve5@outlook.com>
2025-01-23 18:20:21 +0100

    Remove Ditto and El Gato bot implementations; update main.py to exclude these bots from player management

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ from environment.connect4Env import Connect4Env
 from player_manager import Player_Manager
 from enteties.player import Player
 from enteties.bots.randobot import Randobot
-from enteties.bots.ditto import Ditto
 from enteties.bots.randotron import Randotron
-from enteties.bots.georgian import Georgian
-from enteties.bots.el_gato import El_Gato
-from enteties.bots.el_gato_2 import El_Gato_2
 
 
 pygame.init()
@@ -31,16 +27,10 @@ pygame.mouse.set_visible(False)
 player_manager = Player_Manager()
 player_manager.add_player(Player("Human", "assets/player_images/human.png"))
 player_manager.add_player(Randobot("Randobot", "assets/player_images/robot.png"))
-player_manager.add_player(Ditto("Ditto", "assets/player_images/ditto.png"))
 player_manager.add_player(Randotron("Randotron", "assets/player_images/randotron.png"))
-player_manager.add_player(Georgian("Georgian", "assets/player_images/georgian.png", 1))
 
 env = Connect4Env(player_manager)
-
 check_env(env)
-
-#player_manager.add_player(El_Gato("El Gato", "assets/player_images/el_gato.png", env, "el_gato_trained_model.zip"))
-player_manager.add_player(El_Gato_2("El Gato 2", "assets/player_images/el_gato.png", env, "el_gato_2_trained_model.zip"))
 
 vec_env = DummyVecEnv([lambda: env])
 
